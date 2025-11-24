f168558b9e84690e1685d1d1c706a9f6cc376e22
prathix <80820002+prathix@users.noreply.github.com>
2025-11-24 21:35:37 +0000

    a

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install dependencies
-pip install -r requirements.txt
-
-# Run migrations (optional, but often needed)
-python3 manage.py migrate
-
-# Start the Django server
-python3 manage.py runserver 0.0.0.0:$PORT
