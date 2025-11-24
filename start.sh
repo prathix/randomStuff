3b76faf102f6147f35e67eab4446b4671adf48c5
prathix <80820002+prathix@users.noreply.github.com>
2025-11-24 22:32:24 +0100

    Update Python commands to use python3

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 pip install -r requirements.txt
 
 # Run migrations (optional, but often needed)
-python manage.py migrate
+python3 manage.py migrate
 
 # Start the Django server
-python manage.py runserver 0.0.0.0:$PORT
+python3 manage.py runserver 0.0.0.0:$PORT
